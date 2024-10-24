16b5d8f32e26418660b01f7e4965d5a458709059
andrii.kernytskyi <andrii.kernytskyi@rrmechatronics.com>
2024-10-24 17:58:09 +0200

    updated visualization to support model interface

--- src/utils/osmo_visualization.py
+++ src/utils/osmo_visualization.py
@@ -6,54 +6,49 @@ from src.core.models.osmo_model import OsmoModel
 class OsmoVisualizer:
     def __init__(self, model: OsmoModel):
         self.id = model.id
-
-        self.o = model.o
-        self.ei = model.ei
-        self.ei_max = model.ei_max
-        self.o_max = model.o_max
-        self.ei_hyper = model.ei_hyper
-        self.o_hyper = model.o_hyper
-        self.o_min, self.ei_min = model.min
-        self.o_first_peak, self.ei_first_peak = model.first_peak
-        self.area, self.o_segment, self.ei_segment = model.area
+        self.info = model.info
+        self.model = model
 
     def visualize_raw(self):
         _, ax = plt.subplots()
 
         # Plot Osmolality / Elongation raw curve
-        ax.plot(self.o, self.ei,
+        ax.plot(self.model.o, self.model.ei,
                 label='Raw Osmolality vs. Elongation', linewidth=0.75)
 
         # Plot EI / O. first peak
-
-        ax.plot(self.o_first_peak, self.ei_first_peak, 'o',
-                label=f'First peak ({self.o_first_peak}, {self.ei_first_peak})',
+        ax.plot(self.model.o_first_peak, self.model.ei_first_peak, 'o',
+                label=f'First peak ({self.model.o_first_peak}, {self.model.ei_first_peak})',
                 markersize=3)
 
         # Plot EI / O. min
-        ax.plot(self.o_min, self.ei_min, 'o',
-                label=f'EI min ({round(self.o_min)}, {round(self.ei_min, 3)})',
+
+        o_min = self.model.min[0]
+        ei_min = self.model.min[1]
+        ax.plot(o_min, ei_min, 'o',
+                label=f'EI min ({round(o_min)}, {round(ei_min, 3)})',
                 markersize=3)
 
         # Plot EI / O. max
-        ax.plot(self.o_max, self.ei_max, 'o',
-                label=f'EI max ({round(self.o_max)}, {self.ei_max})',
+        ax.plot(self.model.o_max, self.model.ei_max, 'o',
+                label=f'EI max ({round(self.model.o_max)}, {self.model.ei_max})',
                 markersize=3)
 
         # Plot EI / O. hyper
-        ax.plot(self.o_hyper, self.ei_hyper, 'o',
-                label=f'EI hyper ({round(self.o_hyper)}, {round(self.ei_hyper, 3)})',
+        ax.plot(self.model.o_hyper, self.model.ei_hyper, 'o',
+                label=f'EI hyper ({round(self.model.o_hyper)}, {round(self.model.ei_hyper, 3)})',
                 markersize=3)
 
         # Fill the area under the curve
-        plt.fill_between(self.o_segment, self.ei_segment, color='goldenrod',
+        area, o_segment, ei_segment = self.model.area
+        plt.fill_between(o_segment, ei_segment, color='goldenrod',
                          alpha=0.2, hatch='//', edgecolor='black',
-                         label=f'Area = {round(self.area)}')
+                         label=f'Area = {round(area)}')
 
         # Set labels and title
         ax.set_xlabel("Osmolality (O.)")  # X-axis label
         ax.set_ylabel("Elongation Index (EI)")  # Y-axis label
-        ax.set_title(f"{self.id}")
+        ax.set_title(f"{self.info}, {self.id}")
         ax.legend()
         ax.grid(True)  # Add grid for better readability
 
