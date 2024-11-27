482d6378a3da580d293bda60c82a300473436cb6
andrii.kernytskyi <andrii.kernytskyi@rrmechatronics.com>
2024-11-27 01:18:26 +0100

    Drag & Drop Widget. Supports model creation

--- src/controllers/osmo_controller.py
+++ src/controllers/osmo_controller.py
@@ -4,10 +4,11 @@ import os
 
 PLUGINS_FOLDER: str = '../plugins'
 
+from src.utils.osmo_data_loader import load_data
+
 
 class OsmoController:
     def __init__(self):
-        self.data_loader = None
         self.model = None
         self.data_loaded = False
         self.plot_manager = PlotManager()
@@ -16,11 +17,18 @@ class OsmoController:
         plugin_folder = os.path.join(os.getcwd(), PLUGINS_FOLDER)
         self.plugin_manager = PluginManager(self.model, self.plot_manager, plugin_folder)
 
+    def load_file(self, file_path):
+        self.model = load_data(file_path)
+        if self.model is not None:
+            return True
+
     def get_figure(self):
         return self.plot_manager.get_figure()
 
     def get_plugins(self):
-        return self.plugin_manager.discovered_plugins
+        self.plugin_manager.scan_plugins()
+        plugins = self.plugin_manager.discovered_plugins
+        return plugins
 
     def draw_elements(self, elements_ids):
         self.plot_manager.visualize_selected_elements(elements_ids)
