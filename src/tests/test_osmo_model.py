609341d47c8873e075d15437da478cbf7655462a
andrii.kernytskyi <andrii.kernytskyi@rrmechatronics.com>
2024-12-05 11:08:27 +0100

    Updated tests

--- src/tests/test_osmo_model.py
+++ src/tests/test_osmo_model.py
@@ -1,18 +1,11 @@
 import unittest
 import numpy as np
-from src.models.osmo_model import OsmoModel, DataColumnNotFoundError, MissingDataError
+from src.models.osmo_model import OsmoModel
 from src.setup import load_data, load_metadata
 
 
 class TestOsmoModel(unittest.TestCase):
-    # Constants for expected results
-    EXPECTED_O_MAX = 213
-    EXPECTED_EI_MAX = 0.517
-    EXPECTED_O_HYPER = 364
-    EXPECTED_EI_HYPER = 0.259
-    EXPECTED_O_MIN = 114
-    EXPECTED_EI_MIN = 0.228
-    EXPECTED_AREA = 124
+    EXPECTED_UPPER_LIMIT = 450
 
     def setUp(self):
         """Set up test data for each test method."""
@@ -20,56 +13,28 @@ class TestOsmoModel(unittest.TestCase):
         self.metadata = load_metadata()
         self.model = OsmoModel(self.data, self.metadata)
 
-    # Properties Testing
-    def test_get_ei(self):
-        """Test that the 'EI' property returns the correct data."""
-        np.testing.assert_array_equal(self.data["EI"], self.model.get_ei)
+    def test_attribute_access(self):
+        """Test that attribute-like access works for data and metadata."""
+        for key in self.data:
+            # Use numpy.array_equal to compare arrays
+            self.assertTrue(np.array_equal(self.data[key], getattr(self.model, key)))
 
-    def test_get_o_max(self):
-        """Test that o_max returns the expected maximum O value."""
-        rounded_result = round(self.model.get_o_max)
-        self.assertEqual(self.EXPECTED_O_MAX, rounded_result)
+        for key in self.metadata:
+            self.assertEqual(self.metadata[key], getattr(self.model, key))
 
-    def test_get_ei_max(self):
-        """Test that ei_max returns the expected maximum EI value."""
-        self.assertEqual(self.EXPECTED_EI_MAX, self.model.get_ei_max)
+    def test_attribute_error(self):
+        """Test that accessing a non-existent attribute raises an AttributeError."""
+        with self.assertRaises(AttributeError):
+            getattr(self.model, "non_existent_attribute")
 
-    def test_get_o_hyper(self):
-        """Test that o_hyper returns the expected O value."""
-        rounded_result = round(self.model.get_o_hyper)
-        self.assertEqual(self.EXPECTED_O_HYPER, rounded_result)
+    def test_o_attribute_mapping(self):
+        """Test that accessing 'O' returns the correct data mapped from 'O.'."""
+        np.testing.assert_array_equal(self.data["O."], self.model.O)
 
-    def test_get_ei_hyper(self):
-        """Test that ei_hyper returns the expected EI value."""
-        rounded_result = round(self.model.get_ei_hyper, 3)
-        self.assertEqual(self.EXPECTED_EI_HYPER, rounded_result)
-
-    def test_get_o_min(self):
-        """Test that o_min returns the expected O value."""
-        rounded_result = round(self.model.get_o_min)
-        self.assertEqual(self.EXPECTED_O_MIN, rounded_result)
-
-    def test_get_ei_min(self):
-        """Test that o_min returns the expected O value."""
-        self.assertEqual(self.EXPECTED_EI_MIN, self.model.get_ei_min)
-
-    def test_get_area(self):
-        """Test that area returns the expected area value."""
-        # Get area value without plotting segments
-        area, _, _ = self.model.get_area
-        area = round(area)
-        self.assertEqual(self.EXPECTED_AREA, area)
-
-    # Calculations
-    def test_two_points_interpolation(self):
-        # Test data
-        x1, y1, x2, y2 = 10, 10, 20, 20
-
-        value = 15
-        expected_interpolated_ei_value = 15
-
-        result = self.model._interpolate_two_points(value, x1, y1, x2, y2)
-        self.assertEqual(expected_interpolated_ei_value, result)
+    def test_non_existent_metadata_access(self):
+        """Test that accessing non-existent metadata returns an AttributeError."""
+        with self.assertRaises(AttributeError):
+            getattr(self.model, "non_existent_metadata")
 
 
 if __name__ == "__main__":
