1905288cc26f0290f72e3b9d1ac74fdecc9d7561
andrii.kernytskyi <andrii.kernytskyi@rrmechatronics.com>
2024-12-13 16:20:27 +0100

    Added oxy_plugin.py to check if oxy models can be accessed

---
+++ plugins/oxy_plugin.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from src.base_classes.base_plugin import BasePlugin
+
+
+class OxyPlugin(BasePlugin):
+    @property
+    def plugin_name(self):
+        return "OxyPlugin"
+
+    def run_plugin(self, model):
+        """Main entry point for the plugin."""
+        self.model = model
+        self.raw_po2_vs_ei()
+
+    def raw_po2_vs_ei(self):
+        po2 = self.model.pO2
+        ei = self.model.EI
+
+        self.add_line_element(po2, ei, label="pO2 vs EI")
+
